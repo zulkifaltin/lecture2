951785463f69e3640f5f3e2d294e22e9a431486c
Zulkif Altin <zulkif_altin@hotmail.com>
2019-06-27 18:37:17 +0300

    Add functions.py

--- functions.py
+++ functions.py
@@ -1,5 +1,9 @@
 def square(x):
     return x * x
 
-for i in range(10):
-    print("{} squared is {}".format(i, square(i)))
+def main():
+    for i in range(10):
+        print("{} squared is {}".format(i, square(i)))
+
+if __name__ == "__main__":
+    main()
